8bb90da88425f8d708c2256869100652aad51708
Newton1727 <narsing_s@ce.iitr.ac.in>
2026-06-03 19:05:12 +0530

    ssl err

--- civilos-backend/src/config/db.js
+++ civilos-backend/src/config/db.js
@@ -8,6 +8,12 @@ export const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD
   port: env.DB_PORT,
   dialect: "postgres",
   logging: env.NODE_ENV === "development" ? (msg) => logger.debug(msg) : false,
+  dialectOptions: env.NODE_ENV === "production" ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  } : {},
   pool: {
     max: 10,
     min: 0,
